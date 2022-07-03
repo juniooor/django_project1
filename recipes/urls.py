2de02287ff9008d2c7164b918c1fd2f5cab9a60f
juniooor <willams.juniooor@hotmail.com>
2022-07-03 16:48:09 -0300

    criando novas url/views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from recipes.views import home, contato, sobre, testes
+from . import views
 
 
 
 urlpatterns = [
-    path('sobre/', sobre), 
-    path('contato/', contato), 
-    path('', home), 
-    path('testes/',  testes)
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe), # Essa recipe chama a view recipe com unico objeto da lista ex: "recipes/1"
+    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -6,11 +7,7 @@ def home(request):
     return render(request, 'recipes/pages/home.html') 
     # para evitar conflitos de arquivos criar um name space na pasta template
 
-def contato(request):
-    return HttpResponse('contato')
+def recipe(request,id):
+    return render(request, 'recipes/pages/home.html') 
 
-def sobre(request):
-    return HttpResponse('sobre')
-
-def testes(request):
-    return HttpResponse('Toma esse Response distraido')
+    # Nova view para o url seguir
