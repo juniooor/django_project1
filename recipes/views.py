2f92da672ac3ab04ee6e7eb35b31ba14ae9725e2
juniooor <willams.juniooor@hotmail.com>
2022-07-04 17:30:13 -0300

    injetando dados nos cards

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html') 
+    return render(request, 'recipes/pages/home.html', context= { 'recipes': [make_recipe() for _ in range(10)]}) 
     # para evitar conflitos de arquivos criar um name space na pasta template
 
 def recipe(request,id):
-    return render(request, 'recipes/pages/recipe-view.html') 
+    return render(request, 'recipes/pages/recipe-view.html', context= {'recipe':make_recipe()}) 
 
     # Nova view para o url seguir
